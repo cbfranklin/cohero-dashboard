7d01a82de7b54353d5deac0ac1f83d184a57eac3
Connor <connorbfranklin@gmail.com>
2016-11-20 21:57:10 -0500

    polish UI, add simulated events, fix .gitignore

---
+++ index.js
@@ -0,0 +1,106 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var moment = require('moment');
+require('console.table');
+var simulatedEvents = require('./events.js').SimulatedEvents;
+
+app.use('/', express.static('public'))
+
+http.listen(3000, function() {
+    console.log('listening on *:3000');
+});
+
+randomEventLoop();
+
+function randomEvent() {
+
+    var eventType = Math.floor((Math.random() * 2) + 1);
+
+    //puffTaken
+    if (eventType === 1) {
+        var event = eventTemplates.puffTaken;
+        var randomId = Math.floor((Math.random() * 200));
+        event.eventId = 'P-100' + randomId;
+    }
+
+    //lungFunctionTest
+    if (eventType === 2) {
+        var event = eventTemplates.lungFunctionTest;
+        var randomPef = Math.floor((Math.random() * 200) + 400);
+        event.eventDetails.pef = randomPef;
+        var randomId = Math.floor((Math.random() * 200));
+        event.eventId = 'LF-100' + randomId;
+    }
+
+    var now = moment().format('HH:mm:ss');
+    event.localTimestamp = now;
+    event.eventDetails.eventTimeLocal = now;
+
+    var whiskys = ['Glenlivet','Dewars','Jameson','Laphroaig']
+	var kioskName = whiskys[Math.floor(Math.random()*whiskys.length)];
+	event.title = kioskName;
+
+	console.log('\nEvent Emitted\n');
+	console.table(event)
+	io.emit('kioskEvent', event)
+}
+
+function randomEventLoop() {
+    var rand = Math.round(Math.random() * 4000) + 2000;
+    setTimeout(function() {
+        randomEvent();
+        randomEventLoop();
+    }, rand);
+};
+
+var eventTemplates = {};
+
+eventTemplates.puffTaken = {
+    "eventType": "puffTaken",
+    "eventId": "P-12345678",
+    /*Dmitry this will be "P" for Puff, and the Id from the Puff table for puffs, for PFTs it will "LF" for lung function, and the Id from the UserMeasurements table like "LF-678363" */
+    "userId": "18043",
+    "title": "Jameson",
+    /*Will be first name of "account"*/
+    "localTimestamp": "20:11:30",
+    "utcTimestamp": "16:11:30",
+    "eventDetails": { /*This object will be different for puffs vs. lung function tests*/
+        "medication": "Atrovent HFA",
+        "medicationType": "Control",
+        /*This would be either "Control", "Rescue - Emergency", "Rescue - Sport"*/
+        "eventTimeLocal": "20:11:30",
+        "eventTimeUTC": "16:11:30",
+        "syncToDeviceTimeLocal": "20:11:31",
+        "syncToDeviceTimeUTC": "16:11:31",
+        "syncToServerTimeLocal": "20:11:33",
+        "syncToServerTimeUTC": "16:11:33",
+        "inhalerBattery": 98,
+        /*0-100*/
+        "remainingPuffs": 67
+    }
+}
+
+eventTemplates.lungFunctionTest = {
+    "eventType": "lungFunctionTest",
+    "eventId": "LF-456797",
+    /*Dmitry this will be "P" for Puff, and the Id from the Puff table for puffs, for PFTs it will "LF" for lung function, and the Id from the UserMeasurements table like "LF-678363" */
+    "userId": "18041",
+    "title": "Glenlivet",
+    /*Will be first name of "account"*/
+    "localTimestamp": "20:12:30",
+    "utcTimestamp": "16:12:30",
+    "eventDetails": { /*This object will be different for puffs vs. lung function tests*/
+        "pef": 531.22,
+        "fev1": 4.32,
+        "fvc": 326.98,
+        "eventTimeLocal": "20:12:30",
+        "evenTimeUTC": "16:12:30",
+        "syncToDeviceTimeLocal": "20:12:31",
+        "syncToDeviceTimeUTC": "16:12:31",
+        "syncToServerTimeLocal": "20:12:33",
+        "syncToServerTimeUTC": "16:12:33",
+        "battery": 77 /*0-100*/
+    }
+}
--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,210 @@
-$(function(){
-    $(".slideshow > div:gt(0)").hide();
-    setInterval(function() {
-        $('.slideshow > div:first')
-            .hide()
-            .next()
-            .fadeIn(1000)
-            .show()
-            .appendTo('#slideshow');
-    }, 3000);
+var templates = {};
+
+var counts = {
+    "puffTaken": 0,
+    "lungFunctionTest": 0
+};
+
+var data = {
+    "puffTaken": [],
+    "lungFunctionTest": []
+};
+
+var inProgress = {
+    "puffTaken": false,
+    "lungFunctionTest": false
+}
+
+var chrome = /chrom(e|ium)/.test(navigator.userAgent.toLowerCase());
+
+$(function() {
+
+	if (!chrome) {
+        alert('I am designed for use in Google Chrome only.');
+    }
+
+    templates['cohero-queue-item'] = $('.template-cohero-queue-item').html();
+    templates['cohero-notification-puffTaken'] = $('.template-cohero-notification-puffTaken').html()
+    templates['cohero-notification-lungFunctionTest'] = $('.template-cohero-notification-lungFunctionTest').html()
+
+    var socket = io();
+    socket.on('kioskEvent', kioskEventHandler);
+
 });
+
+function kioskEventHandler(msg) {
+    console.log('Event Recieved', msg.eventId);
+    //console.table(msg);
+    addQueueItem(msg);
+}
+
+function addQueueItem(item) {
+    var type = item.eventType;
+    var renderedHTML = Mustache.to_html(templates['cohero-queue-item'], item);
+    var $queue = $('.cohero-queue-' + type);
+    $queue.append(renderedHTML);
+
+    item.queued = true;
+    data[type].push(item)
+    counts[type] += 1;
+
+    setTimeout(checkForQueuedItems, 2000, type)
+
+    console.log('Event Queued', item.eventId);
+}
+
+function removeQueueItem(id) {
+    var $item = $('.cohero-queue-item[data-id="' + id + '"]');
+    $item.slideUp();
+    setTimeout(function() {
+        $item.remove();
+    }, 1000)
+}
+
+function notifyQueuedItem(item) {
+    console.log('notifyQueuedItem', item.eventId)
+    var type = item.eventType;
+    console.log(type)
+
+    if (type === 'puffTaken') {
+        notifyPuffTaken(item)
+    }
+    if (type === 'lungFunctionTest') {
+        notifyLungFunctionTest(item)
+    }
+}
+
+function notifyPuffTaken(item) {
+    inProgress.puffTaken = true;
+    console.log('notifyPuffTaken')
+    var renderedHTML = Mustache.to_html(templates['cohero-notification-puffTaken'], {
+        item: item,
+        count: counts.puffTaken
+    });
+    var $notification = $('.cohero-notification-container-puffTaken');
+    $notification.html(renderedHTML).marquisPuffTaken();
+
+    setTimeout(function() {
+        inProgress.puffTaken = false;
+        checkForQueuedItems('puffTaken')
+    }, 6000)
+}
+
+function notifyLungFunctionTest(item) {
+    inProgress.lungFunctionTest = true;
+    console.log('notifyLungFunctionTest')
+    var renderedHTML = Mustache.to_html(templates['cohero-notification-lungFunctionTest'], {
+        item: item,
+        count: counts.lungFunctionTest
+    });
+    var $notification = $('.cohero-notification-container-lungFunctionTest');
+    $notification.html(renderedHTML).marquisLungFunctionTest();
+
+    setTimeout(function() {
+        inProgress.lungFunctionTest = false;
+        checkForQueuedItems('lungFunctionTest')
+    }, 9000)
+}
+
+function checkForQueuedItems(type) {
+    console.log('determining if in progress', type)
+    if (inProgress[type] === false) {
+        console.log('NOT in progress, checking for queued items')
+            //inProgress[type] = true;
+        var found = false;
+        var dataOfType = data[type];
+
+        for (i in dataOfType) {
+            if (dataOfType[i].queued === true) {
+                var item = dataOfType[i];
+                item.queued = false;
+                console.log('Event DeQueued', item.eventId);
+                notifyQueuedItem(item)
+                removeQueueItem(item.eventId)
+                found = true;
+                break;
+            }
+        }
+
+        //inProgress[type] = false;
+
+        if (found) {
+            setTimeout(checkForQueuedItems, 3000, type);
+        }
+    }
+    if (inProgress[type] === true) {
+        console.log('IN PROGRESS', type)
+    }
+}
+
+
+(function($) {
+    $.fn.marquisPuffTaken = function() {
+        console.log('marquis')
+        console.log($(this).children().eq(0))
+        var $container = $(this);
+        $container.children().hide().eq(0).animateCSS('fadeInUp', {
+            callback: function() {
+                $(this).animateCSS('fadeOutUp', {
+                    delay: 1000,
+                    callback: function() {
+                        $(this).hide();
+                    }
+                })
+            }
+        });
+        setTimeout(function() {
+            console.log($container.children().eq(1))
+            $container.children().eq(1).animateCSS('fadeInUp', {
+                callback: function() {
+                    $(this).animateCSS('fadeOutUp', {
+                        delay: 1000,
+                        callback: function() {
+                            $(this).hide();
+                        }
+                    })
+                }
+            });
+        }, 3000)
+        return this;
+    };
+}(jQuery));
+
+$.fn.marquisLungFunctionTest = function() {
+    var $container = $(this);
+    $container.children().hide().eq(0).animateCSS('fadeInUp', {
+        callback: function() {
+            $(this).animateCSS('fadeOutUp', {
+                delay: 1000,
+                callback: function() {
+                    $(this).hide();
+                }
+            })
+        }
+    });
+    setTimeout(function() {
+        $container.children().eq(1).animateCSS('fadeInUp', {
+            callback: function() {
+                $(this).animateCSS('fadeOutUp', {
+                    delay: 1000,
+                    callback: function() {
+                        $(this).hide();
+                    }
+                })
+            }
+        });
+    }, 3000)
+    setTimeout(function() {
+        $container.children().eq(2).animateCSS('fadeInUp', {
+            callback: function() {
+                $(this).animateCSS('fadeOutUp', {
+                    delay: 1000,
+                    callback: function() {
+                        $(this).hide();
+                    }
+                })
+            }
+        });
+    }, 6000)
+    return this;
+};
